13977be5f44bc26eb3c468af21bd1a8405cb0f9e
Ohh-889 <1509326266@qq.com>
2026-08-09 12:24:01 +0900

    refactor(router): remove duplicated context declarations

--- apps/admin/src/types/router.d.ts
+++ apps/admin/src/types/router.d.ts
@@ -8,11 +8,6 @@ declare global {
     interface MenuExtraRegistry extends Record<import('@/features/menus/extras').ExtraKey, true> {}
 
     interface MenuCategoryRegistry extends Record<import('@/features/menus/menu-category').MenuCategoryKey, true> {}
-
-    interface RouterContext {
-      getHomeRoute: () => RoutePath;
-      homeRoute: RoutePath;
-    }
   }
 }
 
