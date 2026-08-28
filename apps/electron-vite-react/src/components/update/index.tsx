30513f72006fad61592e54eebb1137cc6f07dc28
Ohh-889 <1509326266@qq.com>
2026-08-29 05:28:25 +0900

    fix(electron): 收紧更新完成事件参数类型

--- apps/electron-vite-react/src/components/update/index.tsx
+++ apps/electron-vite-react/src/components/update/index.tsx
@@ -59,7 +59,7 @@ const Update = () => {
     setProgressInfo(arg1);
   }, []);
 
-  const onUpdateDownloaded = useCallback((_event: Electron.IpcRendererEvent, ...args: any[]) => {
+  const onUpdateDownloaded = useCallback((_event: Electron.IpcRendererEvent) => {
     setProgressInfo({ percent: 100 });
     setModalBtn(state => ({
       ...state,
